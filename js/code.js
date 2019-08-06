881c71cc1ed0b3a055c711816e32ba349d71e09a
Lautaro Cepeda <lautac29@gmail.com>
2019-08-06 20:50:01 -0300

    Initial Commit

---
+++ js/code.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    // init animations
+    new WOW().init();
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+
+    // object-fit polyfill run
+    objectFitImages();
+
+    /* init Jarallax */
+    jarallax(document.querySelectorAll('.jarallax'));
+
+    jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+        keepImg: true,
+    });
+
+});
