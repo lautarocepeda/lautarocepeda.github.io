92de4bfc394af656109e50a73c368bf49a071949
Lautaro Cepeda <lautac29@gmail.com>
2019-08-29 21:48:15 -0300

    ended formation section

--- js/code.js
+++ js/code.js
@@ -3,27 +3,35 @@ $(document).ready(function () {
     // init animations
     new WOW().init();
 
-    $('.first-button').on('click', function () {
+    // tooltip init
+    $('[data-toggle="tooltip"]').tooltip()
 
-        $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
-
-        $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
-
-        $('.animated-icon3').toggleClass('open');
-    });
-
-    // object-fit polyfill run
-    objectFitImages();
-
-    /* init Jarallax */
-    jarallax(document.querySelectorAll('.jarallax'));
-
-    jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-        keepImg: true,
-    });
-
+    loadData();
 });
+
+
+function loadData()
+{
+    const info = {
+        name: "Lautaro Cepeda",
+        email: "lautac29@gmail.com",
+        dni: "39236384",
+        birthday: "08/06/1995",
+        city: "Argentina - Mendoza",
+        occupation: "Estudiante",
+        description: 
+            "Waiting for the sun",
+        social: {
+            linkedin: "lautarocrodriguez",
+            github: "lautarocepeda"
+        }
+    }
+
+    $("#name").html(info.name);
+    $("#occupation").html(info.occupation);
+    $("#description").html(info.description);
+
+    /* Social Links */
+    $("#linkedin").attr("href", "http://www.linkedin.com/in/" + info.social.linkedin);
+    $("#github").attr("href", "http://github.com/" + info.social.github);
+}
